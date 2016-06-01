e210f189d70b8cfaebae5b5a0c44ee61c9618cd1
Ovidiu Eremia <ovidiu@edsgrup.ro>
2016-06-01 09:58:41 +0000

    Updated +DROP_Tabele_AL.sql

--- +DROP_Tabele_AL.sql
+++ +DROP_Tabele_AL.sql
@@ -3,6 +3,7 @@ IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AL_Ce
 DROP TABLE [dbo].[AL_Centre_colectare]
 GO
 
+
 /****** Object:  Table [dbo].[AL_Producatori]    Script Date: 5/30/2016 5:49:01 PM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AL_Producatori]') AND type in (N'U'))
 DROP TABLE [dbo].[AL_Producatori]
